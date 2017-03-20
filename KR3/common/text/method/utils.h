d67714649d8878bd252fc4119b1aeb2418fb717f
karikera <karikera3@hotmail.com>
2017-03-20 12:16:18 +0900

    endl (CRLF to LF)

--- KR3/common/text/method/utils.h
+++ KR3/common/text/method/utils.h
@@ -5,8 +5,7 @@ namespace kr
 	template<class Derived, typename Component, class Info>
 	inline OutStream<Derived, Component, Info>& endl(OutStream<Derived, Component, Info>& _Ostr)
 	{
-		static const Component buffer[] = {'\r', '\n'};
-		_Ostr.write(buffer, 2);
+		_Ostr << (Component)'\n';
 		return (_Ostr);
 	}
 }
