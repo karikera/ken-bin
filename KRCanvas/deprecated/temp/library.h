4f773201b95bdc9d015507c6fa7b1a9a9a6a3f7d
karikera <karikera3@hotmail.com>
2017-04-04 18:39:42 +0900

    clean unuse files

--- KRCanvas/deprecated/temp/library.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// js library
-extern "C" void addResizeListener(void(*handlerFunc)(int, int));
-extern "C" void initCanvas();
--- emscripten/stdafx.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <KR3/main.h>
-
-using namespace kr;
-
